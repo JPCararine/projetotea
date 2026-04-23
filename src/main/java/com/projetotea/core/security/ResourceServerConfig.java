c9666b4c6040e14aaa9822432c5ed348196cdc03
João Pedro <joaopedrocararinerangel@gmail.com>
2026-04-23 02:13:18 -0300

    feat(security): implementa resource server com JWT e contexto de usuário autenticado

--- src/main/java/com/projetotea/core/security/ResourceServerConfig.java
+++ src/main/java/com/projetotea/core/security/ResourceServerConfig.java
@@ -1,29 +1,103 @@
 package com.projetotea.core.security;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class ResourceServerConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable())
+        http
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
-                .build();
+                .exceptionHandling(ex -> ex
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                            response.setContentType("application/json");
+                            response.getWriter().write("""
+                                    {
+                                        "status": 403,
+                                        "title": "Acesso negado",
+                                        "detail": "Você não tem permissão para acessar este recurso"
+                                    }
+                                    """);
+                        })
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.setContentType("application/json");
+                            response.getWriter().write("""
+                                    {
+                                        "status": 401,
+                                        "title": "Não autenticado",
+                                        "detail": "Token ausente ou inválido"
+                                    }
+                                    """);
+                        })
+                )
+
+                .csrf(csrf -> csrf.disable())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                );
+
+        return http.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation("http://localhost:8081");
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+            Collection<GrantedAuthority> grantedAuthorities = authoritiesConverter.convert(jwt);
+
+            List<String> authorities = jwt.getClaimAsStringList("authorities");
+            if (authorities == null) {
+                return grantedAuthorities;
+            }
+
+            grantedAuthorities.addAll(authorities
+                    .stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .toList());
+
+            return grantedAuthorities;
+        });
+
+        return converter;
     }
 }
--- src/main/java/com/projetotea/core/security/TeaSecurity.java
+++ src/main/java/com/projetotea/core/security/TeaSecurity.java
@@ -1,4 +1,31 @@
 package com.projetotea.core.security;
 
+import com.projetotea.domain.service.UsuarioPacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
 public class TeaSecurity {
+
+    @Autowired
+    private UsuarioPacienteService usuarioPacienteService;
+
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+    public Long getUsuarioId() {
+        Jwt jwt = (Jwt) getAuthentication().getPrincipal();
+
+        String type = jwt.getClaim("type");
+
+        if(!"user".equals(type)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Apenas usuários podem realizar esse serviço");
+        }
+        return jwt.getClaim("user_id");
+    }
 }
