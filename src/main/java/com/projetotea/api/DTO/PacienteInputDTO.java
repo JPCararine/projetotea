061b4c49129f17cb7349e55d318bd0ff03ae7b1e
João Pedro <joaopedrocararinerangel@gmail.com>
2026-04-27 22:49:07 -0300

    feat: adicionar validações no PacienteInputDTO

--- src/main/java/com/projetotea/api/DTO/PacienteInputDTO.java
+++ src/main/java/com/projetotea/api/DTO/PacienteInputDTO.java
@@ -1,5 +1,8 @@
 package com.projetotea.api.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -9,14 +12,16 @@ import java.time.OffsetDateTime;
 public class PacienteInputDTO {
 
 
+    @NotBlank
     private String nome;
-
+    @NotNull
     private LocalDate dataNascimento;
-
+    @NotBlank
+    @Pattern(regexp = "\\d{11}")
     private String cpf;
 
     private String telefone;
-
+    @NotNull
     private GeneroPacienteInputDTO genero;
 
 
