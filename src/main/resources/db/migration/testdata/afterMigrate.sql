58862f1f0bda7636d9f4070dcd63dd6c99c9e14c
João Pedro <joaopedrocararinerangel@gmail.com>
2026-06-04 02:25:24 -0300

    Corrige CPF da massa de teste

--- src/main/resources/db/migration/testdata/afterMigrate.sql
+++ src/main/resources/db/migration/testdata/afterMigrate.sql
@@ -19,7 +19,7 @@ insert into usuario (id, nome, email, senha, cpf, telefone, categoria, data_cada
     (1, 'Joao Pedro', 'joaop@gmail.com', '$2a$10$Y3gHr1/yp7RSkOxhfMiN6O/psn9tX2pEOosJI6w5xem8Fn.aIBcOi', '52998224725', '11999990001', 'ADMIN', current_timestamp()),
     (2, 'Maria Souza', 'maria@email.com', '$2a$10$Y3gHr1/yp7RSkOxhfMiN6O/psn9tX2pEOosJI6w5xem8Fn.aIBcOi', '15350946056', '11999990002', 'FAMILIAR', current_timestamp()),
     (3, 'Carlos Lima', 'carlos@email.com', '$2a$10$Y3gHr1/yp7RSkOxhfMiN6O/psn9tX2pEOosJI6w5xem8Fn.aIBcOi', '51214739096', '11999990003', 'PROFISSIONAL', current_timestamp()),
-    (4, 'Ana Paula', 'ana@email.com', '$2a$10$Y3gHr1/yp7RSkOxhfMiN6O/psn9tX2pEOosJI6w5xem8Fn.aIBcOi', '00441859000', '11999990004', 'FAMILIAR', current_timestamp()),
+    (4, 'Ana Paula', 'ana@email.com', '$2a$10$Y3gHr1/yp7RSkOxhfMiN6O/psn9tX2pEOosJI6w5xem8Fn.aIBcOi', '14226361008', '11999990004', 'FAMILIAR', current_timestamp()),
     (5, 'Joao Silva', 'joao@email.com', '$2a$10$Y3gHr1/yp7RSkOxhfMiN6O/psn9tX2pEOosJI6w5xem8Fn.aIBcOi', '29537901090', '11999990005', 'PROFISSIONAL', current_timestamp());
 
 insert into paciente_usuario (paciente_id, usuario_id, tipo_relacao) values
